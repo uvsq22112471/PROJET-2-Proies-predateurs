760a40bc8028c9550638d6e607d55b85c7d9f409
uvsq22112471 <99662215+uvsq22112471@users.noreply.github.com>
2022-05-15 21:01:39 +0200

    Update pro-pred_2.0.py
    Marche presque. Encore des problèmes avec l'attente et load

--- pro-pred_2.0.py
+++ pro-pred_2.0.py
@@ -5,37 +5,63 @@ import time
 
 #Parametres
 
-longueur = 5
-largeur = 5
-Npro = 10
-Npred = 5
+longueur = 30
+largeur = 30
+Npro = 25
+Npred = 10
+nbArbres = 10
 Erepro = 10
 AgeMinProies = 3
 AgeMaxProies = 5
 AgeMinPredateurs = 7
 AgeMaxPredateurs = 12
-nbTours = 1
+nbTours = 10
+
+def init():
+    nouveau_terrain = initialisation()
+    grille = [[0 for i in range(N+2)] for j in range(N+2)]
+    for i in range(1, N):
+        x = (i - 1) * LARGEUR_CASE
+        for j in range(1, N):
+            y = (j - 1) * HAUTEUR_CASE
+            if nouveau_terrain[0, i, j] == 0:
+                col = "white"
+            elif nouveau_terrain[0, i, j] == 1:
+                col = "blue"
+            elif nouveau_terrain[0, i, j] == 2:
+                col = "red"
+            else:
+                col = "black"
+            carre = canvas.create_rectangle(x, y, x+LARGEUR_CASE, y+HAUTEUR_CASE, fill=col, outline = "grey50")
+            grille[i][j] = carre
+    return nouveau_terrain
 
 #Creation terrain
+
 def initialisation():
-    """On crée le tableau (avec un tableau numpy, plus élégant d'utilisation que les listes imbriquées et plus optimal pour la mémoire que les listes car non-extensible
-    On aurait tout aussi bien pu avoir, au lieu de "terrain[1, ligne, colonne]" une syntaxe plus classique "terrain[ligne][colonne][1]"."""
+    global nbArbres
     terrain = np.zeros((3, longueur+4, largeur+4))
     #etage 0 : nature de la case (0:libre, 1:proie, 2:predateur, 3:inaccessible)
     #etage 1 : âge (et 3 si case inaccessible pour repère)
     #etage 2 : énergie prédateurs
-    nouveau_terrain = terrain.copy() #On travaille sur les deux terrains en parallèle pour effectuer les modifications
+    nouveau_terrain = terrain.copy()
+    """On crée le tableau (avec un tableau numpy, plus élégant d'utilisation que les listes imbriquées et plus optimal pour la mémoire que les listes car non-extensible
+    On aurait tout aussi bien pu avoir, au lieu de "terrain[1, ligne, colonne]" une syntaxe plus classique "terrain[ligne][colonne][1]"."""
+    arbres = []
+    while nbArbres != 0:
+        arbres.append([rd.randint(2, longueur+2), rd.randint(2, largeur+2)])
+        nbArbres -= 1
     for ligne in range(longueur+4):
         for colonne in range(largeur+4):
-            if ligne == 0 or ligne == longueur+2 or ligne == 1 or ligne == longueur+3 or colonne == 0 or colonne == largeur+2 or colonne == 1 or colonne == largeur+3: #On crée des cases inaccessibles sur les bords
-                nouveau_terrain[0,ligne,colonne] = 3
-                nouveau_terrain[1,ligne,colonne] = 3
-    terrain = nouveau_terrain
-    nouveau_terrain = terrain.copy()
+            if ligne == 0 or ligne == longueur+2 or ligne == 1 or ligne == longueur+3 or colonne == 0 or colonne == largeur+2 or colonne == 1 or colonne == largeur+3 or ([ligne, colonne] in arbres == True): #On crée des cases inaccessibles sur les bords
+                terrain[0,ligne,colonne] = 3
     naissances(terrain, nouveau_terrain, Npro, Npred)
     return nouveau_terrain #les return sont non nécessaires au vu de la structure en tableaux, mais le code est plus lisible ainsi donc nous faisons le choix de les garder
 
+
+
 def naissances(terrain, nouveau_terrain, Nproies, Npred):
+
     """Permet la génération des proies et prédateurs à des positions aléatoires"""
     cases_libres = []
     for ligne in range(longueur+4):
@@ -63,7 +89,9 @@ def naissances(terrain, nouveau_terrain, Nproies, Npred):
         nouveau_terrain[2, ligne_nouveau_predateur, colonne_nouveau_predateur] = rd.randint(5, nouveau_terrain[1, ligne_nouveau_predateur, colonne_nouveau_predateur]-1) #On est bornés par la formule Epre<Apre
     return nouveau_terrain 
 
-def deplacement(ligne, colonne):
+terrain = initialisation()
+nouveau_terrain = terrain.copy() #On travaille sur les deux terrains en parallèle pour effectuer les modifications
+def deplacement(ligne, colonne, terrain, nouveau_terrain):
     """Cette fonction assure le déplacement des animaux.
     Le déplacement est aussi aleatoire que possible"""
     
@@ -151,7 +179,7 @@ def deplacement(ligne, colonne):
         nouveau_terrain[2, ligne, colonne] = 0
     return nouveau_terrain
 
-def manger(ligne, colonne, verslaproie):
+def manger(ligne, colonne, verslaproie, terrain, nouveau_terrain):
     if verslaproie == 0:
         nouveau_terrain[0, ligne-1, colonne-1] = nouveau_terrain[0, ligne, colonne]
         nouveau_terrain[1, ligne-1, colonne-1] = nouveau_terrain[1, ligne, colonne]-1
@@ -209,7 +237,7 @@ def manger(ligne, colonne, verslaproie):
         nouveau_terrain[1, ligne, colonne] = 0
         nouveau_terrain[2, ligne, colonne] = 0
 
-def chasse(ligne, colonne):
+def chasse(ligne, colonne, terrain, nouveau_terrain):
     #nouvelle_case= 42 #valeur juste pour ne pas déclencher d'erreur
     """Correspond au parametre flair de l'enonce.
     Permet les deplacements optimaux des predateurs pour manger des proies
@@ -263,7 +291,7 @@ def chasse(ligne, colonne):
         cases_avec_proie.append([19, 3]) 
         
     if cases_avec_proie == []:
-        deplacement(ligne, colonne)
+        deplacement(ligne, colonne, terrain, nouveau_terrain)
         return nouveau_terrain
     else:
         proies_proches0 = []
@@ -290,7 +318,7 @@ def chasse(ligne, colonne):
             verslaproie = cases_avec_proie[ind][0]
 
         if verslaproie < 8:
-            manger(ligne, colonne, verslaproie)
+            manger(ligne, colonne, verslaproie, terrain, nouveau_terrain)
         
         elif verslaproie == 8:
             cases_acces_dispo = []
@@ -645,7 +673,7 @@ def chasse(ligne, colonne):
                 nouveau_terrain[2, ligne, colonne] = 0
             return nouveau_terrain
     return nouveau_terrain
-def fuite(ligne, colonne):
+def fuite(ligne, colonne, terrain, nouveau_terrain):
     """Permet la fuite optimale des proies quand elles detectent un predateur a proximite
     Remplace deplacement si un predateur est detecte"""
     #le predateur peut sentir la proie
@@ -680,7 +708,7 @@ def fuite(ligne, colonne):
     if nouveau_terrain[0,ligne+1,colonne+1] == 2:
         cases_avec_predateur.append([11, 2])
     if cases_avec_predateur == []:
-        deplacement(ligne, colonne)
+        deplacement(ligne, colonne, terrain, nouveau_terrain)
     else:
         pred_proches0 = []
         pred_proches1 = []
@@ -918,31 +946,29 @@ def fuite(ligne, colonne):
             return nouveau_terrain
     return nouveau_terrain
 
-def etape():
+def etape(terrain, nouveau_terrain):
     """Element répété tout au long du programme combinant vieillissement, déplacement et reproduction"""
-    global terrain, nouveau_terrain
     terrain = nouveau_terrain
+    nouveau_terrain = 0
     nouveau_terrain = terrain.copy()
     #naissances(Fpro, Fpred)
     #terrain = nouveau_terrain
     #nouveau_terrain = terrain.copy()
     for ligne in range(longueur+2):
         for colonne in range(largeur+2):
-            nature_case = terrain[0, ligne, colonne]
-            if nouveau_terrain[1, ligne, colonne] == 0 or (nature_case == 2 and nouveau_terrain[2, ligne, colonne]==0):
+            if nouveau_terrain[1, ligne, colonne] == 0 or (terrain[0, ligne, colonne] == 2 and nouveau_terrain[2, ligne, colonne]==0):
                 nouveau_terrain[0, ligne, colonne] = 0
                 nouveau_terrain[1, ligne, colonne] = 0
                 nouveau_terrain[2, ligne, colonne] = 0
-            if nature_case == 1:
-                reproduction(1, ligne, colonne)
-                fuite(ligne, colonne)
-            elif nature_case == 2:
-                reproduction(2, ligne, colonne)
-                chasse(ligne, colonne)
-    print(nouveau_terrain)
+            if terrain[0, ligne, colonne] == 1:
+                reproduction(1, ligne, colonne, terrain, nouveau_terrain)
+                fuite(ligne, colonne, terrain, nouveau_terrain)
+            elif terrain[0, ligne, colonne] == 2:
+                reproduction(2, ligne, colonne, terrain, nouveau_terrain)
+                chasse(ligne, colonne, terrain, nouveau_terrain)
     return nouveau_terrain
 
-def reproduction(nature, ligne, colonne):
+def reproduction(nature, ligne, colonne, terrain, nouveau_terrain):
     """Assure la reproduction de deux individus de la même espèce placés sur des cases adjacentes"""
     partenaires_potentiels = [] #On cherche si les cases adjacentes abritent un partenaire potentiel
     if terrain[0, ligne-1, colonne] == nature:
@@ -1216,11 +1242,11 @@ def reproduction(nature, ligne, colonne):
 
 #boucle principale :
 
-nouveau_terrain = initialisation()
+"""nouveau_terrain = initialisation()
 while nbTours != 0:
     etape()
     time.sleep(2)
-    nbTours -= 1
+    nbTours -= 1"""
     
 #INTERFACE GRAPHIQUE
 
@@ -1271,3 +1297,123 @@ canvas.grid(row=0, column=1, rowspan=6)
 
 # boucle prinicipale
 racine.mainloop() """
+
+N = longueur
+M = largeur
+
+WIDTH = 700
+HEIGHT = 700
+LARGEUR_CASE = WIDTH // N
+HAUTEUR_CASE = HEIGHT // N
+grille = []
+root = tk.Tk()
+
+root.title("Simulation proies predateurs")
+
+canvas = tk.Canvas(root, bg = "black", height=HEIGHT, width=WIDTH)
+
+map = canvas.create_rectangle(0,0, WIDTH, HEIGHT, fill = "white")
+
+for i in range(N):
+    for j in range(M):
+        grille.append([[0]*N]*M) 
+
+def init():
+    nouveau_terrain = initialisation()
+    grille = [[0 for i in range(N+2)] for j in range(N+2)]
+    for i in range(1, N):
+        x = (i - 1) * LARGEUR_CASE
+        for j in range(1, N):
+            y = (j - 1) * HAUTEUR_CASE
+            if nouveau_terrain[0, i, j] == 0:
+                col = "white"
+            elif nouveau_terrain[0, i, j] == 1:
+                col = "blue"
+            elif nouveau_terrain[0, i, j] == 2:
+                col = "red"
+            else:
+                col = "black"
+            carre = canvas.create_rectangle(x, y, x+LARGEUR_CASE, y+HAUTEUR_CASE, fill=col, outline = "grey50")
+            grille[i][j] = carre
+    return nouveau_terrain
+
+def save_fct():
+    fic = open("save.txt", "w")
+    fic.write(str(nouveau_terrain) + "\n")
+    fic.close()
+
+def load_fct():
+    global N
+    fic = open("save.txt", "r")
+    size = fic.readline()
+    N = len(size)
+    canvas.delete()
+    initialisation()
+    i = j = 0
+    for ligne in fic:
+        nouveau_terrain[ligne] = int(ligne)
+        j += 1
+        if j == N:
+            j = 0
+            i += 1
+    fic.close()
+
+#Boutons
+
+def boucle(terrain, nouveau_terrain, grille):
+    nouveau_terrain = np.array(etape(terrain, nouveau_terrain))
+    time.sleep(.5)
+    for i in range (1, N):
+        for j in range (1, N):
+            if nouveau_terrain[0, i, j] == 0:
+                col = "white"
+            elif nouveau_terrain[0, i, j] == 1:
+                col = "blue"
+            elif nouveau_terrain[0, i, j] == 2:
+                col = "red"
+            else:
+                col = "black"
+            x = (i - 1) * LARGEUR_CASE
+            y = (j - 1) * HAUTEUR_CASE
+            case = canvas.create_rectangle(x, y, x+LARGEUR_CASE, y+HAUTEUR_CASE, fill=col, outline = "grey50")
+            grille[i][j] = case
+    return nouveau_terrain
+
+def start_fct():
+    global nbTours, terrain, nouveau_terrain
+    while nbTours != 0:
+        nouveau_terrain = np.array(boucle(terrain, nouveau_terrain, grille))
+        terrain = nouveau_terrain
+        nouveau_terrain = terrain.copy()
+        nbTours -= 1
+
+generation=tk.Button(root,text="générer terrain",command=init)
+start = tk.Button(root, text="start", command=start_fct) #pour lancer la simulation. il faut mettre les fonctions en relation avec l'interface
+arret=tk.Button(root, text="stop", command=root.destroy) #arret total
+pause=tk.Button(root,text="pause") #mettre en pause
+save=tk.Button(root,text="save", command = save_fct)
+load=tk.Button(root,text="load", command = load_fct)
+
+
+#grid
+
+ 
+
+generation.grid()
+
+start.grid()
+
+save.grid()
+
+load.grid()
+
+arret.grid()
+
+canvas.grid(row = 0, column = 1, rowspan = 6)
+
+ 
+
+#Boucle
+
+root.mainloop()
+
